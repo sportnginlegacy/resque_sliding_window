9a71e6e1a7d06534b4bc7760b066c79bdb5670b3
Jon Phenow <j.phenow@gmail.com>
2013-06-11 10:16:34 -0500

    use extend on plugin

--- lib/resque/plugins/sliding_window.rb
+++ lib/resque/plugins/sliding_window.rb
@@ -1,16 +1,12 @@
 module Resque
   module Plugins
     module SlidingWindow
-      extend ActiveSupport::Concern
+      def before_schedule_sliding_window(*args)
+        ResqueSlidingWindow::WindowSlider.new(self, args).slide
+      end
 
-      module ClassMethods
-        def before_schedule_sliding_window(*args)
-          WindowSlider.new(self, args).slide
-        end
-
-        def after_perform_sliding_window(*args)
-          WindowSlider.new(self, args).close
-        end
+      def after_perform_sliding_window(*args)
+        ResqueSlidingWindow::WindowSlider.new(self, args).close
       end
     end
   end
