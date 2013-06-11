ae9424b53649bda1442c9b3a4fd3751c363bd9c5
Jon Phenow <j.phenow@gmail.com>
2013-06-11 10:16:49 -0500

    use extend on scheduler_patches

--- lib/resque_sliding_window/resque_extensions.rb
+++ lib/resque_sliding_window/resque_extensions.rb
@@ -2,37 +2,34 @@ require 'resque'
 require 'resque_scheduler'
 require 'resque/scheduler'
 module ResqueSlidingWindow
-  module ResqueSchedulerPatches
-    def self.included(base)
-      base.extend ClassMethods
+  module SchedulerPatches
+    def self.extended(base)
       class << base
         alias_method :delayed_timestamp_peek_without_rescue, :delayed_timestamp_peek
         alias_method :delayed_timestamp_peek, :delayed_timestamp_peek_with_rescue
       end
     end
 
-    module ClassMethods
-      def next_item_for_timestamp(timestamp)
-        key = "delayed:#{timestamp.to_i}"
+    def next_item_for_timestamp(timestamp)
+      key = "delayed:#{timestamp.to_i}"
 
-        item = patched_decode redis.lpop(key)
+      item = patched_decode redis.lpop(key)
 
-        # If the list is empty, remove it.
-        clean_up_timestamp(key, timestamp)
-        item
-      end
+      # If the list is empty, remove it.
+      clean_up_timestamp(key, timestamp)
+      item
+    end
 
-      def delayed_timestamp_peek_with_rescue(timestamp, start, count)
-        delayed_timestamp_peek_without_rescue timestamp, start, count
-      rescue Resque::Helpers::DecodeException => e
-        []
-      end
+    def delayed_timestamp_peek_with_rescue(timestamp, start, count)
+      delayed_timestamp_peek_without_rescue timestamp, start, count
+    rescue Resque::Helpers::DecodeException => e
+      []
+    end
 
-      def patched_decode(payload)
-        decode payload
-      rescue Exception => e
-      end
+    def patched_decode(payload)
+      decode payload
+    rescue Exception => e
     end
   end
-  ::Resque.send :include, ResqueSchedulerPatches
 end
+Resque.extend ResqueSlidingWindow::SchedulerPatches
